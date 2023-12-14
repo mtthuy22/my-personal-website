56b7d56abb7776f1bb8cc88b0793dc3d05d0fb5c
Thu Thuy <61125935+mtthuy22@users.noreply.github.com>
2023-12-14 13:09:52 +0100

    refactor: formatted with Prettier

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,23 @@
-'use strict'
+"use strict";
 
-const toggleBtn = document.getElementsByClassName('toggle-btn')[0];
-const menuList = document.getElementsByClassName('menu__list')[0];
+const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
+const menuList = document.getElementsByClassName("menu__list")[0];
 const menuLink = document.querySelectorAll(".menu__link");
 
-toggleBtn.addEventListener('click', () => {
-  menuList.classList.toggle('active');
-  toggleBtn.classList.toggle('active'); 
-  if (toggleBtn.getAttribute('aria-expanded') === 'true'){
-    toggleBtn.setAttribute('aria-expanded', 'false'); 
+toggleBtn.addEventListener("click", () => {
+  menuList.classList.toggle("active");
+  toggleBtn.classList.toggle("active");
+  if (toggleBtn.getAttribute("aria-expanded") === "true") {
+    toggleBtn.setAttribute("aria-expanded", "false");
   } else {
-    toggleBtn.setAttribute('aria-expanded', 'true');
+    toggleBtn.setAttribute("aria-expanded", "true");
   }
 });
 
-menuLink.forEach( n => n.addEventListener('click', closeMenu));
+menuLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    menuList.classList.remove('active');
-    toggleBtn.classList.remove('active');
-    toggleBtn.setAttribute('aria-expanded', 'false');
+  menuList.classList.remove("active");
+  toggleBtn.classList.remove("active");
+  toggleBtn.setAttribute("aria-expanded", "false");
 }
